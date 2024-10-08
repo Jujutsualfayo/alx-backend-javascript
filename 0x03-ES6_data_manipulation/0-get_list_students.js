fe53e5fdf5baf9e65d053489d63ec84e69299850
Jujutsualfayo <alphaspaam@gmail.com>
2024-10-08 07:22:17 +0000

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Obtains a list of students
+ * @author Benjamin ALfayo <alphaspaam@gmail.com>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
