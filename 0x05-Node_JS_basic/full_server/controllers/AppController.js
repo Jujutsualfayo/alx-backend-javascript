e0aeb75759de8a718b86e8161eb9cc9c5511c2cd
Jujutsualfayo <alphaspaam@gmail.com>
2024-11-26 18:19:14 +0000

    updated fullserver

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,12 @@
+/**
+ * Contains theextra  route handlers.
+ * @author Benjamin Alfayo
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
